81d1d18099bf36ce3b4afeb585690ded95545712
Lawmsangi <ddarkphoenixx22@gmail.com>
2024-01-10 22:47:13 +0530

    Add validation for models

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,7 @@
 class Payment < ApplicationRecord
-    belongs_to :author, foreign_key: :author_id, class_name: 'User'
-    has_and_belongs_to_many :categories
+  belongs_to :user, foreign_key: 'author_id'
+  has_and_belongs_to_many :categories
+
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :payments, foreign_key: :author_id
   has_many :categories
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
